c4413f7d380cf0999387f33b2a26d169fc5a3d0c
Efrain Palencia <crazydealer3002@protonmail.com>
2023-05-25 10:11:43 -0400

    Complete overhaul

--- earlyOut/earlyOutList/webDriver/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from webDriver import views
-
-
-urlpatterns = [
-    path('webdriver', views.update_webdriver, name='webdriver'),
-    path('automation', views.run_webdriver, name='automation'),
-]
